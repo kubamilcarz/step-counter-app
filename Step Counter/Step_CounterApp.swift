c68e10048e7ba188be42e49a9ddcb101d20872eb
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-09 16:29:03 +0200

    Initial Commit

---
+++ Step Counter/Step_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_CounterApp.swift
+//  Step Counter
+//
+//  Created by Kuba Milcarz on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Step_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
