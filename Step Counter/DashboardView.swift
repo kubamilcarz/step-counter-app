c169447fff798faeb16aa55eab85616e9c1d3de5
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-12 20:56:28 +0200

    feature: added fetch methods for steps and weight

--- Step Counter/DashboardView.swift
+++ Step Counter/DashboardView.swift
@@ -90,7 +90,6 @@ struct DashboardView: View {
                 }
                 .padding()
                 .task {
-//                    await healthKitManager.addSimulatorData()
                     showPermissionPriming = !hasSeenPermissionPriming
                 }
             }
