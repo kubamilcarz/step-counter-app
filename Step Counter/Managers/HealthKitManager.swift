9e22578bdf4d6a28fbe1c001833e236df903dc01
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-14 23:13:01 +0200

    [feature] steps chart (#3)
    * initial bar chart ui
    
    * step bar chart styling
    
    * dummy data fix
    
    * chart interactivity
    
    * project reorganization - step bar chart
    
    * avg step count calculations
    
    * pie chart basic ui
    
    * pie chart interactivity

--- Step Counter/Managers/HealthKitManager.swift
+++ Step Counter/Managers/HealthKitManager.swift
@@ -15,11 +15,14 @@ final class HealthKitManager {
 
     let types: Set = [HKQuantityType(.stepCount), HKQuantityType(.bodyMass)]
     
+    var stepData: [HealthMetric] = []
+    var weightData: [HealthMetric] = []
+    
     func fetchStepCount() async {
         let calendar = Calendar.current
         let today = calendar.startOfDay(for: .now)
         let endDate = calendar.date(byAdding: .day, value: 1, to: today)!
-        let startDate = calendar.date(byAdding: .day, value: -20, to: endDate)!
+        let startDate = calendar.date(byAdding: .day, value: -28, to: endDate)
         
         let queryPredicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
         let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.stepCount), predicate: queryPredicate)
@@ -31,7 +34,15 @@ final class HealthKitManager {
             intervalComponents: .init(day: 1)
         )
         
-        let stepsCounts = try! await stepsQuery.result(for: store)
+        do {
+            let stepsCounts = try await stepsQuery.result(for: store)
+            
+            stepData = stepsCounts.statistics().map {
+                HealthMetric(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+        } catch {
+            
+        }
     }
     
     func fetchWeightsCount() async {
@@ -50,21 +61,32 @@ final class HealthKitManager {
             intervalComponents: .init(day: 1)
         )
         
-        let weightsCount = try! await weightsQuery.result(for: store)
+        do {
+            let weightsCount = try await weightsQuery.result(for: store)
+            
+            weightData = weightsCount.statistics().map {
+                HealthMetric(
+                    date: $0.startDate,
+                    value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0
+                )
+            }
+        } catch {
+            
+        }
     }
     
     func addSimulatorData() async {
         var mockSamples: [HKQuantitySample] = []
         
-        for i in 0..<20 {
-            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 1_000...20_000))
-            let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: 160 + Double(i/3)...165 + Double(i/5)))
+        for i in 0..<28 {
+            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
+            let weightQuanity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3))))
             
-            let startDate = Calendar.current.date(byAdding: .day, value: -1, to: .now)!
+            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
             let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
-
+            
             let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
-            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuantity, start: startDate, end: endDate)
+            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuanity, start: startDate, end: endDate)
             
             mockSamples.append(stepSample)
             mockSamples.append(weightSample)
---
+++ Step Counter/Screens/DashboardView.swift
@@ -0,0 +1,73 @@
+//
+//  DashboardView.swift
+//  Step Counter
+//
+//  Created by Kuba Milcarz on 09/10/2025.
+//
+
+import Charts
+import SwiftUI
+
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case steps, weight
+
+    var id: Self { self }
+
+    var title: String {
+        switch self {
+        case .steps: return "Steps"
+        case .weight: return "Weight"
+        }
+    }
+}
+
+struct DashboardView: View {
+    @Environment(HealthKitManager.self) private var healthKitManager
+    
+    @State private var selectedStat: HealthMetricContext = .steps
+    @State private var showPermissionPriming: Bool = false
+    @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming: Bool = false
+
+    var isSteps: Bool { selectedStat == .steps }
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    StepBarChart(
+                        selectedStat: selectedStat,
+                        chartData: healthKitManager.stepData
+                    )
+                    
+                    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: healthKitManager.stepData))
+                }
+                .padding()
+            }
+            .task {
+                await healthKitManager.fetchStepCount()
+                showPermissionPriming = !hasSeenPermissionPriming
+            }
+            .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                HealthDataListView(metric: metric)
+            }
+            .sheet(isPresented: $showPermissionPriming) {
+                // fetch health data
+            } content: {
+                HealthKitPermissionPrimingView(hasSeenView: $hasSeenPermissionPriming)
+            }
+        }
+        .tint(isSteps ? .pink : .indigo)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
