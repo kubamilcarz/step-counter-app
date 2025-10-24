0a1dde1982ab6fde7c69a4b21b5865228f6f2616
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-24 22:18:27 +0200

    chart helper suite

--- Step Counter Tests/Step_Counter_Tests.swift
+++ Step Counter Tests/Step_Counter_Tests.swift
@@ -16,3 +16,24 @@ struct Step_Counter_Tests {
         #expect(array.average == 1.8475)
     }
 }
+
+@Suite("Chart Helper Tests")
+struct ChartHelperTests {
+    var metrics: [HealthMetric] = [
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 9))!, value: 1_000),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 10))!, value: 750),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 11))!, value: 500),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 12))!, value: 1_250),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 15))!, value: 250),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 16))!, value: 1_000),
+        .init(date: Calendar.current.date(from: .init(year: 2025, month: 6, day: 17))!, value: 1_500)
+    ]
+    
+    @MainActor @Test func averageWeekdayCount() {
+        let averageWeekdayCount = ChartHelper.averageWeekdayCount(for: metrics)
+        #expect(averageWeekdayCount.count == 4)
+        #expect(averageWeekdayCount[0].value == 1_000.0)
+        #expect(averageWeekdayCount[1].value == 1_125.0)
+        #expect(averageWeekdayCount[2].date.weekdayTitle == "Wednesday")
+    }
+}
