e3c14dce93f0d321eb2fd94f3bbd2c3706b114c9
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-24 23:09:19 +0200

    healthkit priming screen button style

--- Step Counter/Screens/HealthKitPermissionPrimingView.swift
+++ Step Counter/Screens/HealthKitPermissionPrimingView.swift
@@ -21,30 +21,40 @@ struct HealthKitPermissionPrimingView: View {
     """
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 130) {
-            VStack(alignment: .leading, spacing: 10) {
-                Image(.appleHealth)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 90, height: 90)
-                    .shadow(color: .gray.opacity(0.3), radius: 10)
-                    .padding(.bottom, 12)
+        VStack(alignment: .leading, spacing: 10) {
+            Spacer()
+            
+            Image(.appleHealth)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 90, height: 90)
+                .shadow(color: .gray.opacity(0.3), radius: 10)
+                .padding(.bottom, 12)
 
-                Text("Apple Health Integration")
-                    .font(.title2)
-                    .bold()
+            Text("Apple Health Integration")
+                .font(.title2)
+                .bold()
 
-                Text(description)
-                    .foregroundStyle(.secondary)
-            }
-
-            Button("Connect Apple Health") {
+            Text(description)
+                .foregroundStyle(.secondary)
+            
+            Spacer()
+        }
+        .padding(30)
+        .safeAreaInset(edge: .bottom) {
+            Button {
                 showHealthKitPermissions = true
+            } label: {
+                Text("Connect Apple Health")
+                    .padding(8)
+                    .fontWeight(.medium)
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
             .prominentButton(.pink)
             .tint(.pink)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 30)
         }
-        .padding(30)
         .healthDataAccessRequest(
             store: healthKitManager.store,
             shareTypes: healthKitManager.types,
