481d61e0bc2f45383a5ad503250fd546e796b3a5
Jakub Milcarz <kubamilcarz@icloud.com>
2025-10-29 22:02:42 +0100

    refactor: wip data intelligence

--- Step Counter/Managers/HealthIntelligence/HealthIntelligenceRepository.swift
+++ Step Counter/Managers/HealthIntelligence/HealthIntelligenceRepository.swift
@@ -1,5 +1,5 @@
 //
-//  DataAnalyzer.swift
+//  HealthIntelligenceRepository.swift
 //  Step Counter
 //
 //  Created by Kuba Milcarz on 28/10/2025.
@@ -9,21 +9,30 @@ import Foundation
 import FoundationModels
 
 @available(iOS 26.0, *)
-@Observable
-final class DataAnalyzer {
-    static let shared = DataAnalyzer()
+protocol DataIntelligenceRepository: Sendable {
+    var isAvailable: Bool { get }
+    var isThinking: Bool { get }
+    var message: String { get }
+    
+    func analyzeDataStream() -> AsyncThrowingStream<String.PartiallyGenerated, Error>
+}
 
+@available(iOS 26.0, *)
+@Observable
+final class HKIntelligenceRepository: DataIntelligenceRepository {
     let model: SystemLanguageModel = .default
     var coachMessage: String.PartiallyGenerated?
     var isThinking = false
 
-    private init() {}
-
     var isAvailable: Bool {
         model.isAvailable
     }
+    
+    var message: String {
+        String(describing: coachMessage)
+    }
 
-    func analyzeHealthDataStream() -> AsyncThrowingStream<String.PartiallyGenerated, Error> {
+    func analyzeDataStream() -> AsyncThrowingStream<String.PartiallyGenerated, Error> {
         isThinking = true
         coachMessage = nil
 
@@ -70,46 +79,5 @@ final class DataAnalyzer {
                 streamTask.cancel()
             }
         }
-    }
-}
-
-@available(iOS 26.0, *)
-struct HealthDataTool: Tool {
-    var name = "fetchStepsAndWeight"
-    var description = "Fetches the user's recent step count and weight data from HealthKit."
-    
-    private let healthDataRepository: HealthDataRepository
-    
-    init(healthDataRepository: HealthDataRepository) {
-        self.healthDataRepository = healthDataRepository
-    }
-
-    @Generable()
-    struct Arguments {}
-
-    func call(arguments _: Arguments) async throws -> String {
-        let steps = try await healthDataRepository.fetchStepCount().map(\.value)
-        let weights = try await healthDataRepository.fetchWeightsCount(daysBack: 28).map(\.value)
-
-        let stepsHigh = Int(steps.max() ?? 0)
-        let stepsLow = Int(steps.min() ?? 0)
-        let stepsTotal = Int(steps.reduce(0, +))
-        let stepsAverage = Int((Double(stepsTotal) / Double(steps.count)).rounded(.up))
-
-        let weightsHigh = Int(weights.max() ?? 0)
-        let weightsLow = Int(weights.min() ?? 0)
-        let weightDiff = Int(weights.first ?? 0) - Int(weights.last ?? 0)
-
-        return """
-        stepsHighestValue: \(stepsHigh),
-        stepsLowestValue: \(stepsLow),
-        stepsTotalValue: \(stepsTotal),
-        stepsDailyAverageValue: \(stepsAverage),
-        weightHighestValue: \(weightsHigh),
-        weightLowestValue: \(weightsLow),
-        overallWeightDiff: \(weightDiff),
-        numberOfStepDays: \(steps.count)
-        numberOfWeightDays: \(weights.count)
-        """
     }
 }
