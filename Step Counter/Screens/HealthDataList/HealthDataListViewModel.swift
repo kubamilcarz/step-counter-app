7bd0eeda881001845549e2c2c972bae80ba3d509
Jakub Milcarz <kubamilcarz@icloud.com>
2025-11-11 21:16:26 +0100

    health data list view

--- Step Counter/Screens/HealthDataList/HealthDataListViewModel.swift
+++ Step Counter/Screens/HealthDataList/HealthDataListViewModel.swift
@@ -31,15 +31,20 @@ final class HealthDataListViewModel {
         metric = config.metric
         syncListData()
         state = listData.isEmpty ? .initial : .success
+
+        logService?.trackEvent(event: Event.viewAppeared(metric: metric, itemCount: listData.count))
     }
 
     func onAddSuccess() {
         syncListData()
         state = listData.isEmpty ? .initial : .success
+
+        logService?.trackEvent(event: Event.addCompleted(metric: metric, itemCount: listData.count))
     }
 
     func onAddButtonTapped() {
         showAddDataSheet = true
+        logService?.trackEvent(event: Event.addTapped(metric: metric))
     }
 
     private func syncListData() {
@@ -50,6 +55,40 @@ final class HealthDataListViewModel {
         switch metric {
         case .steps: healthDataStore.stepData
         case .weight: healthDataStore.weightData
+        }
+    }
+}
+
+extension HealthDataListViewModel {
+    enum Event: LoggableEvent {
+        case viewAppeared(metric: HealthMetricContext, itemCount: Int)
+        case addTapped(metric: HealthMetricContext)
+        case addCompleted(metric: HealthMetricContext, itemCount: Int)
+
+        var eventName: String {
+            switch self {
+            case .viewAppeared: "HealthDataList_View_Appeared"
+            case .addTapped: "HealthDataList_Add_Tapped"
+            case .addCompleted: "HealthDataList_Add_Completed"
+            }
+        }
+
+        var parameters: [String: Any]? {
+            switch self {
+            case let .viewAppeared(metric, itemCount):
+                ["metric": metric.rawValue, "itemCount": itemCount]
+            case let .addTapped(metric):
+                ["metric": metric.rawValue]
+            case let .addCompleted(metric, itemCount):
+                ["metric": metric.rawValue, "itemCount": itemCount]
+            }
+        }
+
+        var type: LogType {
+            switch self {
+            case .addTapped: .info
+            default: .analytic
+            }
         }
     }
 }
